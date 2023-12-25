5661fbce6bfc4d0c79e02b6ebd5727c41364bbed
arkablok <ihpazlagasukanda2403@gmail.com>
2023-12-25 22:39:04 +0700

    memperbaiki url gambar

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -6,9 +6,9 @@
       <div class="container">
           <div class="hero__item set-bg" data-setbg="{{ asset('frontend/img/hero/banner.jpg') }}">
               <div class="hero__text">
-                  <span>FRUIT FRESH</span>
-                  <h2>Vegetable <br />100% Organic</h2>
-                  <p>Free Pickup and Delivery Available</p>
+                  <span>Produk Original</span>
+                  <h2>Produk Otomotif <br />100% Original</h2>
+                  <p>Free Ongkir and Diskon Available</p>
                   <a href="#" class="primary-btn">SHOP NOW</a>
               </div>
           </div>
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -2,11 +2,11 @@
 <html lang="zxx">
   <head>
     <meta charset="UTF-8" />
-    <meta name="description" content="Ogani Template" />
-    <meta name="keywords" content="Ogani, unica, creative, html" />
+    <meta name="description" content="Otomartif Template" />
+    <meta name="keywords" content="Otomartif, unica, creative, html" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Ogani | Template</title>
+    <title>Otomatif | Otomatif Market</title>
 
     <!-- Google Font -->
     <link
